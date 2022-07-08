d76a5a2f56a5460acf2bcd4af94e5cf5f76b4c22
Rajkumar Thangavel <rajkumarthangavel@Rajkumars-MacBook-Air.local>
2022-07-08 22:54:42 +0530

    Flames UI is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { match } from "./Utils";
 
 function App() {
+  const [names, setNames] = useState("");
+  const [crush_name, changeCrushName] = useState("");
+  const [result, setResult] = useState(null);
+  const [showResult, setShowResult] = useState(false);
+
+  const calculate = () => {
+    if (names && crush_name) {
+      let result = match(names, crush_name);
+      setResult(result);
+      setNames("");
+      changeCrushName("");
+    }
+  };
+
+  useEffect(() => {
+    if (result) {
+      setShowResult(true);
+    }
+  }, [result]);
+
+  const flamesRes = () => {
+    switch (result) {
+      case "f":
+        return "😎 Friend 😎";
+      case "l":
+        return "❤️ Love ❤️";
+      case "a":
+        return "🥰 Affection 🥰";
+      case "m":
+        return "💍 Marriage 💍";
+      case "e":
+        return "😈 Enemy 😈";
+      case "s":
+        return "👧 Sister 👧";
+      default:
+        return "";
+    }
+  };
+
+  const ShowContent = () => {
+    if (showResult) {
+      return (
+        <>
+          <br />
+          <br />
+          <h1 className="final_result">{flamesRes(showResult)}</h1>
+        </>
+      );
+    }
+    return (
+      <p className="user_registered-text">
+        Flames is the combination of 6 letters and the word flames has not
+        specific meaning. So, F is Friends, L is Lovers, A is Attraction, M is
+        Marriage, E is Enemies and S is Siblings. This funny game is played by
+        almost every 90’s kid in their school. They used to play on the paper
+        those days
+      </p>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="user">
+        <div className="user_options-container">
+          <div className="user_options-text">
+            <div className="user_options-registered">
+              <button className="user_registered-login" id="login-button">
+                Love is Love 🌈
+              </button>
+              <h2 className="user_registered-title">FLAMES</h2>
+              <ShowContent />
+            </div>
+          </div>
+
+          <div className="user_options-forms" id="user_options-forms">
+            <div className="user_forms-login">
+              <h2 className="forms_title">FLAMES CALCULATOR</h2>
+              <fieldset className="forms_fieldset">
+                <div className="forms_field">
+                  <input
+                    type="text"
+                    placeholder="Enter your Name"
+                    className="forms_field-input"
+                    onChange={(e) => setNames(e.target.value)}
+                    value={names}
+                    required
+                  />
+                </div>{" "}
+                <br />
+                <div className="forms_field">
+                  <input
+                    type="text"
+                    onChange={(e) => changeCrushName(e.target.value)}
+                    value={crush_name}
+                    placeholder="Enter your Crush Name ❤️"
+                    className="forms_field-input"
+                    required
+                  />
+                </div>
+                <button className="forms_buttons-action" onClick={calculate}>
+                  CALCULATE
+                </button>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/Utils.js
+++ src/Utils.js
@@ -10,8 +10,8 @@ const blockSwap = (arr, d, n) => {
   return result;
 };
 
-let flames_num = [1, 2, 3, 4, 5, 6];
-const flamesCalc = (count) => {
+// let flames_num = [1, 2, 3, 4, 5, 6];
+const flamesCalc = (count, flames_num) => {
   if (flames_num.length > 1) {
     let remainder;
     if (count > flames_num.length) {
@@ -21,13 +21,13 @@ const flamesCalc = (count) => {
     }
     flames_num.splice(remainder - 1, 1);
     flames_num = blockSwap(flames_num, remainder - 1, flames_num.length);
-    return flamesCalc(count);
+    return flamesCalc(count,flames_num);
   } else {
     return flames_arr[flames_num[0] - 1];
   }
 };
 
-const match = (str1, str2) => {
+export const match = (str1, str2) => {
   let str1_arr = str1.split("");
   let str2_arr = str2.split("");
   for (let i = 0; i < str1_arr.length; i++) {
@@ -38,7 +38,5 @@ const match = (str1, str2) => {
       }
     }
   }
-  return flamesCalc(str1_arr.length + str2_arr.length);
+  return flamesCalc(str1_arr.length + str2_arr.length, [1, 2, 3, 4, 5, 6]);
 };
-
-console.log(match("rajkumar", "jeyasri"));
